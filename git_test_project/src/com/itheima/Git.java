e18079b5dd03ae0b6934b4cc3ab6923c489566e2
liuzhilong <747854453@qq.com>
2018-11-24 21:40:42 +0800

    idea中第一次提交git项目工程

---
+++ git_test_project/src/com/itheima/Git.java
@@ -0,0 +1,20 @@
+package com.itheima;
+
+/**
+ * @author pulid
+ * @date 2018/11/24
+ */
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("git的测试");
+        System.out.println("git的测试");
+        System.out.println("git的测试");
+        System.out.println("git的测试");
+        System.out.println("git的测试");
+        System.out.println("git的测试");
+        System.out.println("git的测试");
+        System.out.println("git的测试");
+        System.out.println("git的测试");
+    }
+}
